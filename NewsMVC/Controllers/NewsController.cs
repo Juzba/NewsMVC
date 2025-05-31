6b4eed353a0977c62c1af8c9af35d70e1e5164e2
Jiří Strnadel <Juzba88@gmail.com>
2025-05-31 20:25:28 +0200

    Přidejte soubory projektu.

---
+++ NewsMVC/Controllers/NewsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewsMVC.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult News()
+        {
+            return View();
+        }
+    }
+}
